1d03b548788f3bfc557897776baa1345697416e7
Chathura <chathura.madura11@gmail.com>
2022-06-03 02:38:51 +0530

    create header component

---
+++ src/common/Header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> The Branded Shop</label>
+
+          </div>
+          <div className='right row RText'>
+            <label></label>
+            <label>Need Help?</label>
+            <span></span>
+            <label></label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import Search from "./Search"
+import Navbar from "./Navbar"
+import Head from "./Head"
+
+const Header = ({ CartItem }) => {
+
+  return (
+    <>
+    <Head/>
+      <Search CartItem={CartItem} />
+      <Navbar />
+    </>
+  )
+}
+
+export default Header
