a0977ff20eb2c47a6959dfa6036bf198e409b934
Chathura <chathura.madura11@gmail.com>
2022-06-03 02:38:15 +0530

    create app component and add header , footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
 import './App.css';
 
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./common/Header/Header";
+// import Pages from "./pages/Pages"
+// import Data from "./components/Data"
+// import Cart from "./common/Cart/Cart"
+import Footer from "./common/Footer/Footer";
+// import Sdata from "./components/shops/Sdata"
+
 function App() {
+  const [CartItem, setCartItem] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header CartItem={CartItem} />
+        <Routes>
+          {/* <Route path="/" exact>
+            <Pages
+              productItems={productItems}
+              addToCart={addToCart}
+              shopItems={shopItems}
+            />
+          </Route> */}
+
+          {/* <Route path="/cart" exact>
+            <Cart
+              CartItem={CartItem}
+              addToCart={addToCart}
+              decreaseQty={decreaseQty}
+            />
+          </Route> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
