50c418a90cf2f946fddc0bea9200bf8ea656f372
Chathura <chathura.madura11@gmail.com>
2022-06-04 02:37:16 +0530

    add Sdata to sidebar component

---
+++ src/components/MainPage/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    id: 1,
+    title: "50% Off For Your First Shopping",
+    desc: "Women striped colour blocked Maxi Dress",
+    cover: "./images/SlideCard/slide-1.png",
+  },
+  {
+    id: 2,
+    title: "50% Off For Your First Shopping",
+    desc: "Women Maroon Three-Quarter Sleeves Floral Printed Maxi Dress",
+    cover: "./images/SlideCard/slide-2.png",
+  },
+  {
+    id: 3,
+    title: "50% Off For Your First Shopping",
+    desc: "Women's Solid Long Dress",
+    cover: "./images/SlideCard/slide-3.png",
+  },
+  {
+    id: 4,
+    title: "50% Off For Your First Shopping",
+    desc: "Women red solid sleeveless party dress",
+    cover: "./images/SlideCard/slide-4.png",
+  },
+]
+export default Sdata
