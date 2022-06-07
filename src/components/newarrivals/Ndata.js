3ad04852a61938de568765db3c55eaef51c23776
Chathura <chathura.madura11@gmail.com>
2022-06-07 23:48:38 +0530

    add new arrival, shop item data

---
+++ src/components/newarrivals/Ndata.js
@@ -0,0 +1,34 @@
+const Ndata = [
+  {
+    cover: "./images/arrivals/arrivals1.png",
+    name: "Women Blue Solid Round Neck T-shirt",
+    price: "1500",
+  },
+  {
+    cover: "./images/arrivals/arrivals2.png",
+    name: "Men Ash Printed Slim Fit Round Neck T-Shirt",
+    price: "2500",
+  },
+  {
+    cover: "./images/arrivals/arrivals3.png",
+    name: "Men Black Printed Slim Fit Round Neck T-Shirt",
+    price: "1750",
+  },
+  {
+    cover: "./images/arrivals/arrivals4.png",
+    name: "Women's Striped Linen Dress",
+    price: "15",
+  },
+  {
+    cover: "./images/arrivals/arrivals5.png",
+    name: "Men's Cream Printed Casual Shirt",
+    price: "1800",
+  },
+  {
+    cover: "./images/arrivals/arrivals6.png",
+    name: "Men's Batik Sarong",
+    price: "400",
+  },
+]
+
+export default Ndata
---
+++ src/components/shops/Sdata.js
@@ -0,0 +1,47 @@
+const Sdata = {
+  shopItems: [
+    {
+      id: 7,
+      cover: "./images/shops/shops-1.png",
+      name: "Women Ballerinas",
+      price: "180",
+      discount: "25",
+    },
+    {
+      id: 8,
+      cover: "./images/shops/shops-2.png",
+      name: "Unisex Sneaker",
+      price: "120",
+      discount: "10",
+    },
+    {
+      id: 9,
+      cover: "./images/shops/shops-3.png",
+      name: "Women Sandals",
+      price: "20",
+      discount: "50 ",
+    },
+    {
+      id: 10,
+      cover: "./images/shops/shops-4.png",
+      name: "Women Block Heels",
+      price: "999",
+      discount: "10 ",
+    },
+    {
+      id: 11,
+      cover: "./images/shops/shops-5.png",
+      name: "Women Sandals",
+      price: "80",
+      discount: "20 ",
+    },
+    {
+      id: 12,
+      cover: "./images/shops/shops-7.png",
+      name: "Redimi Phone",
+      price: "400",
+      discount: "20 ",
+    }
+  ],
+}
+export default Sdata
