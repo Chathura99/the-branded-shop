b8147b550fd0752414f5f1f4deabf6b627b45020
Chathura <chathura.madura11@gmail.com>
2022-06-11 19:03:05 +0530

    crate footer component

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,27 +1,25 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container grid2'>
-          <div className='box'>
+        <div className="container grid2">
+          <div className="box">
             <h1>The Branded-Shop</h1>
-            <p>For over 5 years, The Branded SHop clothes and garment have been synonymous with luxury, quality and premium selection. As the premier designer, manufacturer, and supplier of luxurious clothes, apparels such as vintage printed or embroidered t-shirts, jacket, for the entire wholesale and retail, industries, our capabilities are endless when it comes to creating the perfect quality merchandise.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div>
+            <p>
+              For over 5 years, The Branded SHop clothes and garment have been
+              synonymous with luxury, quality and premium selection. As the
+              premier designer, manufacturer, and supplier of luxurious clothes,
+              apparels such as vintage printed or embroidered t-shirts, jacket,
+              for the entire wholesale and retail, industries, our capabilities
+              are endless when it comes to creating the perfect quality
+              merchandise.
+            </p>
           </div>
 
-          <div className='box'>
+          <div className="box">
             <h2>About Us</h2>
             <ul>
               <li>Careers</li>
@@ -31,17 +29,8 @@ const Footer = () => {
               <li>Privacy Policy</li>
             </ul>
           </div>
-          <div className='box'>
-            <h2>Customer Care</h2>
-            <ul>
-              <li>Help Center </li>
-              <li>How to Buy </li>
-              <li>Track Your Order </li>
-              <li>Corporate & Bulk Purchasing </li>
-              <li>Returns & Refunds </li>
-            </ul>
-          </div>
-          <div className='box'>
+
+          <div className="box">
             <h2>Contact Us</h2>
             <ul>
               <li>12, Kegalle Road, Polgahawela </li>
@@ -52,7 +41,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
