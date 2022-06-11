4d715fe8c85ee4b87e29fb62536c5bce744242a9
Chathura <chathura.madura11@gmail.com>
2022-06-11 19:07:32 +0530

    make categories component

---
+++ src/components/MainPage/Catdata.jsx
@@ -0,0 +1,36 @@
+const data = [
+    {
+      cateImg: "./images/category/cat1.png",
+      cateName: "T-shirt",
+    },
+    {
+      cateImg: "./images/category/cat2.png",
+      cateName: "Shorts",
+    },
+    {
+      cateImg: "./images/category/cat3.png",
+      cateName: "Skirts",
+    },
+    {
+      cateImg: "./images/category/cat4.png",
+      cateName: "Tops",
+    },
+    {
+      cateImg: "./images/category/cat5.png",
+      cateName: "Dresses",
+    },
+    {
+      cateImg: "./images/category/cat6.png",
+      cateName: "Sweaters",
+    },
+    {
+      cateImg: "./images/category/cat7.png",
+      cateName: "Casual Trousers",
+    },
+    {
+      cateImg: "./images/category/cat8.png",
+      cateName: "Sarong",
+    }
+  ]
+
+  export default data
---
+++ src/components/MainPage/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "./Catdata";
+const Categories = () => {
+  return (
+    <>
+      <div className="category">
+        {data.map((value, index) => {
+          return (
+            <div className="box f_flex" key={index}>
+              <img src={value.cateImg} alt="" />
+              <span>{value.cateName}</span>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
